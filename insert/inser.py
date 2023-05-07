c6560fd4a2154350c4576658bb4fc643ac04dd9a
zekraquraan <zekraquraan7@gmail.com>
2023-05-07 18:39:34 +0300

    6:39

--- insert/inser.py
+++ insert/inser.py
@@ -1,16 +1,25 @@
-def insertShiftArray(arr,x):
-    '''
-    This function inserts an element into an array 
-    and shifts the other elements to make place for it.
-    :param arr: list
-    :param x: int
-    :return: list
-    '''
-    midArr=len(arr) // 2
-    if len(arr) % 2 == 0:
-      arr.insert(midArr, x)
-    else:
-       int(midArr)
-       arr.insert(midArr+1, x)
+import math
+
+def swap(arr, x, y):
+    temp = arr[x]
+    arr[x] = arr[y]
+    arr[y] = temp
+
+def insert(arr, num):
+    if len(arr)<2:
+        arr.append(num)
+        return arr
+    
+    
+    size = len(arr)
+    arr.append(num)
+    
+    i = size
+    while i > math.ceil(size / 2):
+        swap(arr, i, i-1)
+        i -= 1
+        
     return arr
-  
+    
+print(insert([42, 18, 15, 42], 55))
+print(insert([2, 4, 6,-8], 5))
--- insert/insert.md
+++ insert/insert.md
@@ -1,11 +1,16 @@
 # Challenge Title
-<!-- Description of the challenge -->
+## array-insert-shift
 
 ## Whiteboard Process
-<!-- Embedded whiteboard image -->
 
+[whiteboard](./wh22.png)
 ## Approach & Efficiency
-<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+## Insert and shift an array in middle at index
+# Time Complexity
+# O(n) because we loop n/2 times and when our input is large the performance will be similar to looping n times
+
+# Space Complexity
+# O(1) because regardless of the size of our input, the algorithm only allocates a constant number of variables
 
 ## Solution
-<!-- Show how to run your code, and examples of it in action -->
+# just run the following code [](./inser.py)
