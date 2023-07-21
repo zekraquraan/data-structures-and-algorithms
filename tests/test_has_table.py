cb0147e00f67037b5e240e8015b9ffb0d1f3153f
zekraquraan <zekraquraan7@gmail.com>
2023-07-21 23:15:28 +0000

    2:15

---
+++ tests/test_has_table.py
@@ -0,0 +1,17 @@
+
+'''
+'d' -> 100 * 283 = 28300 % 1024 = 652
+'''
+
+from hash_table.hash_table import HashTable
+
+
+
+def test_hash_method():
+  expected = 652
+  hash = HashTable()
+  actual = hash._HashTable__hash('d')
+  assert expected == actual
+
+
+# (DS._DataScience__PrintMeNot()
